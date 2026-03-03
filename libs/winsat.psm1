9c7c7c60765fd6cbba0280ddb93d42d641249c14
fsteltenkamp <florian@steltenkamp.de>
2026-03-03 11:35:08 +0100

    winsat library (WIP)

---
+++ libs/winsat.psm1
@@ -0,0 +1,201 @@
+<#
+    .SYNOPSIS
+        Library for WinSAT Testing
+
+    .DESCRIPTION
+        Provides functions for running the Windows System Assessment Tool (WinSAT) and retrieving its results.
+
+    .NOTES
+        Author  : Florian Steltenkamp
+        Version : 1.0
+        Url     : https://github.com/fsteltenkamp/powershell-libs
+        Exitcodes:
+        - 1: General error
+#>
+
+$global:xmlFilePath = "C:\winsat.xml"
+$global:saveAsXml = $true
+$global:jsonFilePath = "C:\winsat.json"
+$global:saveAsJson = $false
+
+function Set-XmlFileLocation {
+    <#
+    .SYNOPSIS
+        Sets the location of the WinSAT XML results file.
+    .PARAMETER Path
+        The path to the WinSAT XML results file.
+    #>
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+    $global:xmlFilePath = $Path
+    log "info" "WinSAT XML file location set to '$Path'."
+}
+
+function Enable-XmlOutput {
+    <#
+    .SYNOPSIS
+        Enables saving WinSAT results as XML.
+    #>
+    $global:saveAsXml = $true
+    log "info" "WinSAT XML output enabled."
+}
+
+function Disable-XmlOutput {
+    <#
+    .SYNOPSIS
+        Disables saving WinSAT results as XML.
+    #>
+    $global:saveAsXml = $false
+    log "info" "WinSAT XML output disabled."
+}
+
+function Set-JsonFileLocation {
+    <#
+    .SYNOPSIS
+        Sets the location of the WinSAT JSON results file.
+    .PARAMETER Path
+        The path to the WinSAT JSON results file.
+    #>
+    param(
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+    $global:jsonFilePath = $Path
+    log "info" "WinSAT JSON file location set to '$Path'."
+}
+
+function Enable-JsonOutput {
+    <#
+    .SYNOPSIS
+        Enables saving WinSAT results as JSON.
+    #>
+    $global:saveAsJson = $true
+    # due to winsat not supporting JSON output natively, we will convert the XML output to JSON after the assessment is completed
+    $global:saveAsXml = $true # ensure XML output is enabled to have the source data for JSON conversion
+    log "info" "WinSAT JSON output enabled."
+}
+
+function Disable-JsonOutput {
+    <#
+    .SYNOPSIS
+        Disables saving WinSAT results as JSON.
+    #>
+    $global:saveAsJson = $false
+    log "info" "WinSAT JSON output disabled."
+}
+
+function Convert-ToJson {
+    <#
+    .SYNOPSIS
+        Converts the WinSAT XML results to JSON format.
+    #>
+    if (Test-Path -Path $global:xmlFilePath) {
+        try {
+            $xmlContent = Get-Content -Path $global:xmlFilePath
+            $jsonContent = $xmlContent | ConvertTo-Json
+            Set-Content -Path $global:jsonFilePath -Value $jsonContent
+            log "info" "WinSAT results converted to JSON and saved to '$global:jsonFilePath'."
+        } catch {
+            log "error" "An error occurred while converting WinSAT XML to JSON: $_"
+        }
+    } else {
+        log "error" "WinSAT XML file not found at '$global:xmlFilePath'. Cannot convert to JSON."
+    }
+}
+
+function Run-WinSAT {
+    <#
+    .SYNOPSIS
+        Runs the WinSAT assessment with specified arguments.
+    .PARAMETER Arguments
+        The arguments to pass to the WinSAT command.
+    #>
+    param(
+        [Parameter(Mandatory)]
+        [string]$Arguments
+    )
+    try {
+        $winSatPath = "$env:windir\system32\winsat.exe"
+        if (Test-Path -Path $winSatPath) {
+            log "info" "Running WinSAT with arguments: $Arguments"
+            Start-Process -FilePath $winSatPath -ArgumentList $Arguments -Wait -NoNewWindow
+            log "info" "WinSAT assessment completed."
+        } else {
+            log "error" "WinSAT executable not found at '$winSatPath'."
+        }
+    } catch {
+        log "error" "An error occurred while running WinSAT: $_"
+    }
+}
+
+function Run-FullWinSAT {
+    <#
+    .SYNOPSIS
+        Runs the full WinSAT assessment.
+    #>
+    $winSatArgs = "formal -restart clean -v"
+    if ($global:saveAsXml) {
+        $winSatArgs += " -xml $global:xmlFilePath"
+    }
+    Run-WinSAT -Arguments $winSatArgs
+}
+
+function Run-WinSATDiskTest {
+    <#
+    .SYNOPSIS
+        Runs the WinSAT disk performance test.
+    .DESCRIPTION
+        This function runs the WinSAT disk performance test, which assesses the read and write performance of the system's storage devices.
+        If a drive letter is provided, it will test that specific drive. Otherwise, it will test all drives.
+    .PARAMETER DriveLetter
+        The drive letter to test (e.g., "C:"). If not provided, all drives will be tested.
+    #>
+    param(
+        [string]$DriveLetter = $null
+    )
+    $winSatArgs = "disk -seq -read -write"
+    if ($null -ne $DriveLetter) {
+        $winSatArgs += " -drive $DriveLetter"
+    }
+    Run-WinSAT -Arguments $winSatArgs
+}
+
+function Get-WinSATResults {
+    <# 
+    .SYNOPSIS
+        Retrieves the WinSAT results in the specified format (XML or JSON).
+    .PARAMETER Format
+        The format to retrieve the results in ("xml" or "json").
+    #>
+    param(
+        [Parameter(Mandatory)]
+        [ValidateSet("xml", "json")]
+        [string]$Format
+    )
+    if ($Format -eq "xml") {
+        # return content of the xml file if it exists, otherwise log an error and return null
+        if (Test-Path -Path $global:xmlFilePath) {
+            # return content of the xml file
+            return Get-Content -Path $global:xmlFilePath
+        } else {
+            log "error" "WinSAT XML file not found at '$global:xmlFilePath'."
+            return $null
+        }
+    } elseif ($Format -eq "json") {
+        # convert xml to json, save it in the file, remove the xml file.
+        Convert-ToJson
+        if (Test-Path -Path $global:jsonFilePath) {
+            # remove xml file:
+            Remove-Item -Path $global:xmlFilePath -ErrorAction SilentlyContinue
+            # return content of the json file
+            return Get-Content -Path $global:jsonFilePath
+        } else {
+            log "error" "WinSAT JSON file not found at '$global:jsonFilePath'."
+            return $null
+        }
+    }
+}
+
+Export-ModuleMember -Function Set-XmlFileLocation, Enable-XmlOutput, Disable-XmlOutput, Set-JsonFileLocation, Enable-JsonOutput, Disable-JsonOutput, Convert-ToJson, Run-WinSAT, Run-FullWinSAT, Get-WinSATResults
--- test.ps1
+++ test.ps1
@@ -14,7 +14,7 @@
 #>
 
 # Define libraries to import:
-$libs = @("logger", "file", "crypt", "util", "http")
+$libs = @("crypt", "file", "http", "logger", "util", "winsat")
 # Download and import the Updater Script:
 $updaterUrl = "https://raw.githubusercontent.com/fsteltenkamp/powershell-libs/main/updater.ps1"
 $updaterPath = "$PSScriptRoot\updater.ps1"
@@ -24,19 +24,6 @@ Import-Module $updaterPath -Force
 Update-Libs -Libs $libs
 
 # Test if the libraries were imported successfully by calling a function from each library:
-
-# Logger library test:
-try {
-    setLogLevel -Level "debug"
-    enableLogfile -FilePath "$PSScriptRoot\testlog.txt"
-    log "info" "This is a test log message."
-    if (-not (Test-Path -Path "$PSScriptRoot\testlog.txt")) {
-        throw "Failed to create log file."
-    }
-    Write-Host "Logger library functions are working."
-} catch {
-    Write-Host "Error: Logger library functions are not working."
-}
 
 # crypt library test:
 try {
@@ -48,17 +35,6 @@ try {
     Write-Host "Get-StringHash function from crypt library is working."
 } catch {
     Write-Host "Error: Get-StringHash function from crypt library is not working."
-}
-
-# util library test:
-try {
-    $publicIp = Get-PublicIp
-    if ($null -eq $publicIp.ipv4) {
-        throw "Failed to retrieve public IPv4 address."
-    }
-    Write-Host "Get-PublicIp function from util library is working. Public IPv4: $($publicIp.ipv4)"
-} catch {
-    Write-Host "Error: Get-PublicIp function from util library is not working."
 }
 
 # file library test:
@@ -82,4 +58,41 @@ try {
     Write-Host "New-RequestHeaders function from http library is working. Headers: $($headers.raw | Format-List)"
 } catch {
     Write-Host "Error: New-RequestHeaders function from http library is not working."
+}
+
+# Logger library test:
+try {
+    setLogLevel -Level "debug"
+    enableLogfile -FilePath "$PSScriptRoot\testlog.txt"
+    log "info" "This is a test log message."
+    if (-not (Test-Path -Path "$PSScriptRoot\testlog.txt")) {
+        throw "Failed to create log file."
+    }
+    Write-Host "Logger library functions are working."
+} catch {
+    Write-Host "Error: Logger library functions are not working."
+}
+
+# util library test:
+try {
+    $publicIp = Get-PublicIp
+    if ($null -eq $publicIp.ipv4) {
+        throw "Failed to retrieve public IPv4 address."
+    }
+    Write-Host "Get-PublicIp function from util library is working. Public IPv4: $($publicIp.ipv4)"
+} catch {
+    Write-Host "Error: Get-PublicIp function from util library is not working."
+}
+
+# winsat library test:
+try {
+    Enable-JsonOutput
+    Run-WinSATDiskTest -DriveLetter "C"
+    $winsatResults = Get-WinSATResults
+    if ($null -eq $winsatResults) {
+        throw "Failed to retrieve WinSAT results."
+    }
+    Write-Host "Get-WinSATResults function from winsat library is working. Results: $($winsatResults | Format-List)"
+} catch {
+    Write-Host "Error: Get-WinSATResults function from winsat library is not working."
 }
